23b1de6e97294150b38030fd2062d04ece1f38db
WnareaJunior <44757327+WnareaJunior@users.noreply.github.com>
2022-07-06 13:37:45 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch({headless : false});
+
+    const page = await browser.newPage();
+    await page.goto("https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149");
+    const title = await page.title;
+    const url = await page.url;
+    console.log(title,url);
+    
+})();
